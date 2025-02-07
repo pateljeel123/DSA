3251979ce5e3b953ea4ede54a3815be40a1c85dc
pateljeel123 <148971534+pateljeel123@users.noreply.github.com>
2025-02-07 15:08:15 +0000

    updated

---
+++ day4/1_to_n.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    
+    
+    int a=1;
+    int n;
+    cout << "N:";
+    cin >> n;
+    
+    
+    
+    while(a<=n){
+        
+        cout << a << endl;
+        a++;
+    }
+    
+    return 0;
+}
