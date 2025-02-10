871d3c6084164ece0a980cd186b3a0788074b2f2
pateljeel123 <148971534+pateljeel123@users.noreply.github.com>
2025-02-10 04:28:22 +0000

    updated

---
+++ day4/leap_years.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+  int start=2000;
+  int end=3000;
+  for(int i=start; i<=end; i+=4){
+      cout << i << " ";
+      
+      
+  }
+   return 0; 
+}
