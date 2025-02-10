181595c4fb55c0f388a550084ea851aeccb9e40e
pateljeel123 <148971534+pateljeel123@users.noreply.github.com>
2025-02-10 03:54:12 +0000

    updated

---
+++ day4/ even_number_n_to_1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+    
+ int end=1;
+ int a=5;
+ 
+ for(int i=17;i>=end;i--){
+     
+     if(i%2==0){
+         cout << i << endl;
+     }
+      
+ }
+
+    return 0;
+}
