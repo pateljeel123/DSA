4b21f541732f00fe1df68618e85ec4652f3716de
pateljeel123 <148971534+pateljeel123@users.noreply.github.com>
2025-02-11 11:21:56 +0000

    done

---
+++ day6/project10.cpp
@@ -0,0 +1,17 @@
+// 544, 509, 474, 439, ...
+#include <iostream>
+using namespace  std;
+int main() {
+    
+    int start=544;
+    
+    for(int i=1;i<=6;i++){
+        cout  << start <<  " " ;
+        start-=35;
+         
+        
+       
+    }
+ 
+    return 0;
+}
---
+++ day6/project9.cpp
@@ -0,0 +1,17 @@
+//201, 202, 204, 207
+#include <iostream>
+using namespace  std;
+int main() {
+    
+    int start=201;
+    
+    for(int i=1;i<=6;i++){
+        cout  << start <<  " " ;
+        start+=i;
+         
+        
+       
+    }
+ 
+    return 0;
+}
