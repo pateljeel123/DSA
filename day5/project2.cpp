dcd555015d309e31548dc0ba3f5545b8ab7185f2
pateljeel123 <148971534+pateljeel123@users.noreply.github.com>
2025-02-11 05:47:16 +0000

    updated

---
+++ day5/project2.cpp
@@ -0,0 +1,18 @@
+// 7, 10, 8, 11, 9, 12, ...
+
+#include <iostream>
+using namespace std;
+
+int main() {
+       
+       int start=7;
+       int start2=10;
+       
+       for(int i=7;i<=10;i++){
+           cout << i << endl ;
+            cout << start2 << endl ;
+           start2++;
+       }
+
+    return 0;
+}
