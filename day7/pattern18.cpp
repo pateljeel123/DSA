edde199497d3d5adff27d75b2436c376427835f3
pateljeel123 <148971534+pateljeel123@users.noreply.github.com>
2025-02-13 08:16:28 +0000

    updated

---
+++ day7/pattern18.cpp
@@ -0,0 +1,23 @@
+// 0
+// 0 1
+// 0 1 0
+// 0 1 0 1
+// 0 1 0 1 0
+
+#include<iostream>
+using namespace  std;
+
+int main(){
+    for(int i=1;i<=5;i++){//123
+        for(int j=1;j<=i;j++){
+            if(j%2==0){
+                cout <<"1 ";
+            }
+            else{
+               cout << "0 "; 
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
