80cc3d4d99d61c73883c3bd56ad10c857e5e6d30
pateljeel123 <148971534+pateljeel123@users.noreply.github.com>
2025-02-13 05:14:05 +0000

    updated

---
+++ day7/pattern3.cpp
@@ -0,0 +1,18 @@
+// 1
+// 22
+// 333
+// 4444
+// 55556
+
+#include<iostream>
+using namespace  std;
+
+int main(){
+    for(int i=1;i<=5;i++){
+        for(int j=1;j<=i;j++){
+            cout << i;
+        }
+        cout << endl;
+    }
+    return 0;
+}
---
+++ day7/pattern4.cpp
@@ -0,0 +1,18 @@
+// 5 4 3 2 1
+// 5 4 3 2
+// 5 4 3
+// 5 4
+// 5
+
+#include<iostream>
+using namespace  std;
+
+int main(){
+    for(int i=1;i<=5;i++){//123
+        for(int j=5;j>=i;j--){
+            cout << j;
+        }
+        cout << endl;
+    }
+    return 0;
+}
