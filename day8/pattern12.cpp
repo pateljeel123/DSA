f0e6e8bedd0623ebf22a9a3aae0adc474270a590
pateljeel123 <148971534+pateljeel123@users.noreply.github.com>
2025-02-18 06:01:30 +0000

    done

---
+++ day8/pattern12.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+ 
+//         * 
+//       * * 
+//     * * * 
+//   * * * * 
+// * * * * *
+
+  
+ 
+int main(){
+    for(int i=1;i<=5;i++){
+        for(int k=i;k<5;k++){
+            cout << "  ";
+        }
+        for(int j=i;j>=1;j--){
+            cout << "*" << " ";
+        }
+        cout << endl;
+        
+    }
+    return 0;
+}
+
