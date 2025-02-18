f0d6633833c38250ef6292b30a5e04bfc62eb4eb
pateljeel123 <148971534+pateljeel123@users.noreply.github.com>
2025-02-18 07:34:04 +0000

    done

---
+++ day8/pattern19.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+ 
+// 1  0  1  0  1  
+//   0  1  0  1  
+//       1  0  1  
+//          0  1  
+//             1
+  
+ 
+int main(){
+    for(int i=1;i<=5;i++){
+        for(int k=1;k<i;k++){
+            cout << "   ";
+        }
+        for(int j=i;j<=5;j++){
+           if(j%2==1){
+               cout << "1  ";
+           }
+           else{
+               cout << "0  ";
+           }
+        }
+        cout << endl;
+        
+    }
+    return 0;
+}
+
