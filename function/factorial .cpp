6cf1047b8156b226dcb5791b9f4f485b3ad21ed7
pateljeel123 <148971534+pateljeel123@users.noreply.github.com>
2025-02-28 09:40:39 +0000

    done

---
+++ function/factorial .cpp
@@ -0,0 +1,18 @@
+// wap to find factorial user passed number using udf.
+
+#include <iostream>
+using namespace std;
+
+int factorial(int n) {
+    int fact = 1;
+    for (int i = 1; i <= n; i++) {
+        fact *= i;
+    }
+    return fact;
+}
+
+int main() {
+    int num;
+    cin >> num;
+    cout << factorial(num);
+}
